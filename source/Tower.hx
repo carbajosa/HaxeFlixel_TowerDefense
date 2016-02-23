4adc4f5334cf3b1b2bc4d65f3f4256b23c3d7039
Don Siegfried Carbajosa <doncarbajosaoriginal@gmail.com>
2016-02-24 06:50:56 +0800

    v0.1.1.1
    Code cleaning to make Tower.hx prepared for dynamic data.

--- source/Tower.hx
+++ source/Tower.hx
@@ -1,80 +1,51 @@
 package;
-
 import flixel.FlxSprite;
-import flixel.system.FlxAssets.FlxGraphicAsset;
-import flixel.math.FlxVelocity;
 import flixel.math.FlxMath;
+import haxe.Log;
 
 /**
- * ...
- * @author ...
+ * @author don
  */
 class Tower extends FlxSprite
 {
+	// Public Members
+	public var range:Float;
+	public var fireRate:Float;
+	public var damage:Float;
+	public var currentTarget:Enemy;
 	
-	private var range:Float;
-	private var damage:Float;
-	private var fireRate:Float;
+	// Private Members
+	private var defaultRange:Float;
+	private var defaultFireRate:Float;
 	private var fireCooldown:Float;
-	private var currentTarget:Enemy;
-
-	public function new(?X:Float=0, ?Y:Float=0) 
+	private var defaultDamage:Float;
+	private var autoTargetLock:Bool;
+	
+	public function new(?X:Float = 0, ?Y:Float = 0)
 	{
 		super(X, Y);
-		
-		range = 400;
-		fireRate = 0.75;
-		fireCooldown = 0;
-		currentTarget = null;
-		damage = 3;
-		
-		makeGraphic(32, 32);
+		init();
+	}
+	
+	public function init(?Damage:Float=1.0, ?FireRate:Float=1.0, ?Range:Float=150, ?AutoTargetlock:Bool=false):Void
+	{
+		range = defaultRange = Range;
+		fireRate = defaultFireRate = FireRate;
+		damage = defaultDamage = Damage;
+		autoTargetLock = AutoTargetlock;
 	}
 	
 	override public function update(elapsed:Float):Void 
 	{
 		currentTarget = getEnemy();
-		
-		if (canFire(elapsed) && currentTarget != null && currentTarget.alive)
+		fireCooldown = fireCooldown < fireRate? fireCooldown += elapsed : fireRate;
+		if (fireCooldown >= fireRate && currentTarget != null && currentTarget.alive)
 		{
 			fireCooldown = 0;
 			fire();
 		}
 		
-		
 		super.update(elapsed);
-	}
-	
-	private function getEnemy():Enemy
-	{
-		var _target = null;
-		
-		for (enemy in Reg.PS.enemyGroup)
-		{
-			var distance = FlxMath.distanceBetween(this, enemy);
-			if (distance < range && enemy.alive)
-			{
-				_target = enemy;
-				break;
-			}
-		}
-		
-		return _target;
-	}
-	
-	private function canFire(elapsed:Float):Bool
-	{
-		if (fireCooldown <= fireRate)
-		{
-			fireCooldown += elapsed;
-		}
-		
-		if (fireCooldown >= fireRate)
-		{
-			return true;
-		} else {
-			return false;
-		}
 	}
 	
 	private function fire():Void
@@ -85,4 +56,22 @@ class Tower extends FlxSprite
 		bullet.init(mid.x, mid.y, currentTarget, damage, 400);
 	}
 	
+	// -------------------------------------------------------------------------
+	// Helper Functions
+	// -------------------------------------------------------------------------
+	private function getEnemy():Enemy {
+		var _target = null;
+		
+		for (enemy in Reg.PS.enemyGroup)
+		{
+			var _distance = FlxMath.distanceBetween(this, enemy);
+			if (_distance <= range && enemy.alive)
+			{
+				_target = enemy;
+				if (autoTargetLock) break;
+			}
+		}
+		
+		return _target;
+	}
 }
